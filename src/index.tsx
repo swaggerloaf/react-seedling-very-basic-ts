8c4ca851123db498c7aeb0f1e7640587470d4ee4
swaggerloaf <swaggerloaf@gmail.com>
2021-12-29 22:25:07 -0500

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-const rootElement = document.getElementById("root");
+import './styles.scss';
 
+const initState = { count: 0 };
+
+type ACTIONTYPE =
+  | { type: 'increment'; payload: number }
+  | { type: 'decrement'; payload: string };
+
+function reducer(state: typeof initState, action: ACTIONTYPE) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload };
+    case 'decrement':
+      return { count: state.count - Number(action.payload) };
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = React.useReducer(reducer, initState);
+
+  return (
+    <div className="text-danger">
+      Count: {state.count}
+      <button
+        className="btn btn-danger btn-lg"
+        onClick={() => dispatch({ type: 'decrement', payload: '5' })}
+      >
+        -
+      </button>
+      <button
+        className="btn btn-warning btn-lg"
+        onClick={() => dispatch({ type: 'increment', payload: 5 })}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
-root.render(<div>Glenn</div>);
 
-//ReactDOM.render(<div>Glenn</div>, rootElement);
+root.render(<Counter />);
