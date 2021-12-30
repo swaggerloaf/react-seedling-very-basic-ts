334f1db25c711c8402014527f0ad15b7a73e1ca5
swaggerloaf <swaggerloaf@gmail.com>
2021-12-29 23:13:01 -0500

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,63 @@
-import React, { useReducer } from 'react';
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import './styles.scss';
 
-interface Todo {
-  title: string;
-  description: string;
-}
-
-const todo: Partial<Todo> = {
-  title: 'Call Fred'
-};
-
-const initState = { count: 0 };
-
-type ACTIONTYPE =
-  | { type: 'increment'; payload: number }
-  | { type: 'decrement'; payload: string };
-
-function reducer(state: typeof initState, action: ACTIONTYPE) {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + action.payload };
-    case 'decrement':
-      return { count: state.count - Number(action.payload) };
-    default:
-      throw new Error();
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* toto */}</button>;
   }
 }
 
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initState);
+class Board extends React.Component {
+  renderSquare(i: number) {
+    i;
+    return <Square />;
+  }
 
-  return (
-    <div className="text-danger">
-      Count: {state.count}
-      <button
-        className="btn btn-danger btn-lg"
-        onClick={() => dispatch({ type: 'decrement', payload: '5' })}
-      >
-        -
-      </button>
-      <button
-        className="btn btn-primary btn-lg"
-        onClick={() => dispatch({ type: 'increment', payload: 5 })}
-      >
-        +
-      </button>
-    </div>
-  );
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById('root')!;
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<Counter />);
+root.render(<Game />);
